37e17308c6eeacbfcfa09d2218f3eaefcc287378
Jacob Walser <jwalser90@gmail.com>
2019-06-29 23:35:00 -0400

    wip

--- ci/travis-ci-script.sh
+++ ci/travis-ci-script.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env/ bash
+
 python setup.py install --user || exit 1
 python brping/pingmessage.py || exit 1
 ci/test-ci.py || { pip install pyOpenSSL --user && ci/test-ci.py; } || exit 1
