bdd48c0300ebac3f31e27e441e512e859fc44e54
Jacob Walser <jwalser90@gmail.com>
2019-06-29 23:44:48 -0400

    wip

--- ci/travis-ci-script.sh
+++ ci/travis-ci-script.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+echo "installing lib..."
 python setup.py install || exit 1
+
+echo "testing message api..."
 python brping/pingmessage.py || exit 1
-ci/test-ci.py || { pip install pyOpenSSL --user && ci/test-ci.py; } || exit 1
-ci/update-gh-pages.sh || exit 1
+
+echo "update gh pages..."
+pip install pyOpenSSL && ci/update-gh-pages.sh || exit 1
