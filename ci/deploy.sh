73934e28acbb87c169b3f688e078ff1acfec8e9e
Jacob Walser <jwalser90@gmail.com>
2019-07-01 14:54:52 -0400

    wip

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -8,7 +8,7 @@ test cat ci/deploy-whitelist
 cat ci/deploy-whitelist | xargs git add -f
 git commit -m temporary-commit
 test git checkout deployment
-test git checkout HEAD@{1}
+cat ci/deploy-whitelist | xargs git checkout HEAD@{1}
 test git diff --staged
 test git commit -m "update auotgenerated files"
 test git log
