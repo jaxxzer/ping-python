0fc86ec3c2e49d7a361daec8d75eb096e52b1455
Jacob Walser <jwalser90@gmail.com>
2019-07-01 15:20:10 -0400

    wip

--- ci/ci-functions.sh
+++ ci/ci-functions.sh
@@ -10,10 +10,10 @@ echob() {
 
 test() {
     echob "$@"
-    echo -en 'travis_fold:start:#{testN}\\r'
+    echo -en 'travis_fold:start:#{$testN}\\r'
     "$@"
-    echo -en 'travis_fold:start:#{testN}\\r'
-    $testN=$testN + 1
+    echo -en 'travis_fold:start:#{$testN}\\r'
+    testN=$((testN+1))
     exitcode=$?
     echob "$@ exited with $exitcode"
     if [ $exitcode -ne 0 ]; then exit $exitcode; fi
