08afed683d43bb9c8e25abc8c3cb2afbcfe547fa
Jacob Walser <jwalser90@gmail.com>
2019-07-01 12:35:39 -0400

    wip

--- ci/travis-ci-script.sh
+++ ci/travis-ci-script.sh
@@ -3,18 +3,18 @@
 . ci/ci-functions.sh
 
 echob "generating message api..."
-citest pip install jinja2
-citest generate/generate-python.py --output-dir=brping
+test pip install jinja2
+test generate/generate-python.py --output-dir=brping
 
 echob "testing message api..."
-citest python brping/pingmessage.py
+test python brping/pingmessage.py
 
 echob "installing package..."
-citest python setup.py install
+test python setup.py install
 
 echob "update gh pages..."
-citest pip install pyOpenSSL
-citest ci/update-gh-pages.sh
+test pip install pyOpenSSL
+test ci/update-gh-pages.sh
 
 echob "deploying..."
-citest ci/deploy.sh
+test ci/deploy.sh
