60b754803f7b041bc2d5be18f54769182d2247b8
Jacob Walser <jwalser90@gmail.com>
2019-07-01 12:46:34 -0400

    wip

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,6 +4,7 @@
 
 test git branch -a
 test git branch
+test cat ci/deploy-whitelist
 test cat ci/deploy-whitelist | xargs git add -f
 test git commit -m temporary-commit
 test git checkout deployment
