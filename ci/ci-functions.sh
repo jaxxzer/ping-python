947ace0ce87100fb1fe7b6ad90482560453a03a2
Jacob Walser <jwalser90@gmail.com>
2019-07-01 15:18:12 -0400

    wip

--- ci/ci-functions.sh
+++ ci/ci-functions.sh
@@ -2,6 +2,7 @@
 
 bold=$(tput bold)
 normal=$(tput sgr0)
+testN=0
 
 echob() {
     echo "${bold}${@}${normal}"
@@ -9,7 +10,10 @@ echob() {
 
 test() {
     echob "$@"
+    echo -en 'travis_fold:start:#{testN}\\r'
     "$@"
+    echo -en 'travis_fold:start:#{testN}\\r'
+    $testN=$testN + 1
     exitcode=$?
     echob "$@ exited with $exitcode"
     if [ $exitcode -ne 0 ]; then exit $exitcode; fi
